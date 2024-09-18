eea4526eeed915888023b53bb5a3f1b6b1663f66
msanrub359 <msanrub359@gmail.com>
2024-09-18 12:59:41 +0200

    actualización app.js depuración

--- UD1/07-Depuración/js/app.js
+++ UD1/07-Depuración/js/app.js
@@ -6,20 +6,20 @@
 let nombre = Juan; 
 
 // 2. Imprimir el nombre en la consola
-console.log("El nombre de la persona es: " + nombre);
+console.log(`El nombre de la persona es: ${nombre} `);
 
 // 3. Declarar variables para almacenar la edad y la altura
 let edad = 20,
     altura = "170cm"; 
 
-// 4. Intentar sumar edad y altura
+// 4. Intentar sumar edad y altura`
 const total = edad + altura; 
-console.log("Suma de edad y altura: " + total); 
+console.log(`Suma de edad y altura: ${total}` ); 
 
 // 5. Intentar corregir la suma convirtiendo altura a número
 const alturaNumerica = parseInt(altura); 
 const sumaCorregida = edad + alturaNumerica;
-console.log("Suma corregida de edad y altura: " + sumaCorregida); 
+console.log(`Suma corregida de edad y altura: {sumaCorregida}`); 
 
 // 6. Uso de condicionales para comprobar si una persona es mayor de edad
 if (edad = 18) { 
@@ -31,15 +31,15 @@ if (edad = 18) {
 }
 
 // 7. Contar del 1 al 5
-for (var i = 0; i < 5; i++) {
-    console.log("Contador: " + i); 
+for (let i = 0; i < 5; i++) {
+    console.log(`Contador: {i}`); 
 }
 
 
 
 // 8. Intentar acceder a una variable 
-console.log("El valor de la variable 'apellido' es: " + apellido); // Error: 'apellido' no está definida
+console.log(`El valor de la variable 'apellido' es: { apellido}`); // Error: 'apellido' no está definida
 
 // 10. Corregir un número decimal mal formateado
-var precio = 20,5; 
-console.log("El precio es: " + precio);
+let precio = 20,5; 
+console.log(`El precio es: {precio}`);
