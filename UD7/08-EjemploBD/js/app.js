25b7376858cea9b89d30cf94a7237784909fa261
Mª Luz Sanchez Rubio <45260715+msanrub359@users.noreply.github.com>
2025-05-15 12:39:45 +0200

    Update app.js

--- UD7/08-EjemploBD/js/app.js
+++ UD7/08-EjemploBD/js/app.js
@@ -9,7 +9,50 @@ const Can = (() => {
     }
 
     const mostrar=(e)=> {
+        let param="perro=";
+        if (e.target.id == "first"){
+            param+= '111A';
+        }
+
        
+        fetch (`./php/perros.php?${param}`)
+        .then (response=>{
+            if (response.status==200){
+                return response.json();
+            }else{
+                Swal.fire("El fichero no existe")
+            }
+        })
+        .then(datos=>{
+            
+            //eliminar filas
+            // document.querySelector("tbody").querySelectorAll("tr").forEach(elemento=>{
+            //     elemento.remove(); //elimina la fila
+            // });
+            document.querySelector("tbody").innerText=""
+           
+            const {data} = datos
+            //console.log(data)
+            data.forEach(elemento=>{
+                const fila = document.createElement("tr");
+                const colChip=document.createElement("td");
+                colChip.textContent=elemento.chip;
+                const colNombre=document.createElement("td");
+                colNombre.textContent=elemento.nombre;
+                const colRaza=document.createElement("td");
+                colRaza.textContent=elemento.raza;
+                const colFechaNac=document.createElement("td");
+                colFechaNac.textContent=elemento.fechaNac;
+
+                fila.append(colChip,colNombre,colRaza, colFechaNac);
+                document.querySelector("tbody").append(fila)
+            })
+        })
+        .catch(error=>{
+            Swal.fire("Error comunicación con el servidor")
+            
+        })
+      
       
     
     }
